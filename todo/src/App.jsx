08b1fef37c665d9da7e5ff4e4ae597582069163a
Saniya Saifi <saniyasaifi912@gmail.com>
2026-07-15 10:41:28 +0530

    add todo app submit

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,122 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Plus, Trash2 } from "lucide-react";
+import "./App.css";
+import { v4 as uuid } from "uuid";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = { id: uuid(), task: input, completed: false };
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
+
+  const handleCompleted = (id) => {
+    const updatedTodos = todos.map((obj) => {
+      if (obj.id === id) {
+        return { ...obj, completed: !obj.completed };
+      }
+      return obj;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const handleDelete = (id) => {
+    const updatedTodos = todos.filter((obj) => obj.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  //hook
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
+    <div className="App">
+      <h1 className="heading font">TODO</h1>
+
+      <h6 className="font">Manage your day!</h6>
+
+      <form onSubmit={handleSubmit} className="form">
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">
+          <Plus className="icon" />
         </button>
-      </section>
+      </form>
 
-      <div className="ticks"></div>
+      <div className="todos_container">
+        {todos.length === 0 && (
+          <div className="tips">
+            Add Your First To-Do Item! <br />
+            📝 Usage Tips 💡: <br /> ✔️ Press Enter to submit actions. <br /> ✔️ Drag to reorder your to-dos (PC only) <br />{" "}
+            ✔️ Double-click to edit slogan and tasks. <br /> ✔️ Access quick actions in the right sidebar. <br /> 🔒 Your
+            data is stored locally in your browser. <br /> 📝 Supports data download and import.
+          </div>
+        )}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        {todos.map((todo, index) => {
+          return (
+            <div key={index} className="todo_item">
+              <input checked={todo.completed} type="radio" onChange={() => handleCompleted(todo.id)} />
+              <p>{todo.task}</p>
+              <Trash2 onClick={() => handleDelete(todo.id)} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+export default App;
